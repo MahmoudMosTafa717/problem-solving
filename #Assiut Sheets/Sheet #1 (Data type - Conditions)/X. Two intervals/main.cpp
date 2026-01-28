11f87cb4a360e64712b31df64534e4de42c7d6fe
Mahmoud MosTafa <mhmodq717@gmail.com>
2026-01-29 00:47:09 +0200

    X. Two intervals

--- #Assiut Sheets/Sheet #1 (Data type - Conditions)/X. Two intervals/main.cpp
+++ #Assiut Sheets/Sheet #1 (Data type - Conditions)/X. Two intervals/main.cpp
@@ -4,6 +4,32 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    long long L1,R1,L2,R2;
+    cin >> L1 >> R1 >> L2 >> R2;
+
+    if (R1 < L2 || R2 < L1){
+        cout << -1;
+    }
+
+    else if( (L1>=L2 && R1 <= R2))
+    {
+     cout <<  L1 << " " << R1;
+    }
+
+    else if( (L1>=L2 && R1 >= R2))
+    {
+     cout << L1 << " " << R2;
+    }
+
+    else if( (L1<=L2 && R1 >= R2))
+    {
+     cout << L2 << " " << R2;
+    }
+
+    else if( (L1<=L2 && R1 <= R2))
+    {
+     cout << L2 << " " << R1;
+    }
+
     return 0;
 }
